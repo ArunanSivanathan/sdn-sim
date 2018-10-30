bd0fffa27e20764efcc6243ab4d11aeb7fb7ded5
Arunan Sivanathan <a.sivanathan@unsw.edu.au>
2018-10-30 17:56:37 +1100

    URL updated

--- downloadall.sh
+++ downloadall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget http://iotanalytics.unsw.edu.au/iotFiles/filelist.txt -O filelist.txt
+wget https://iotanalytics.unsw.edu.au/iottestbed/pcap/filelist.txt -O filelist.txt
 cat filelist.txt | egrep -v "(^#.*|^$)" | xargs -n 1 wget
